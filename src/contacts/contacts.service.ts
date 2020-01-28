2ef2fef2782895f68aa5fd3f9b239128925ddb24
Dark Angel <jonathanyombo@gmail.com>
2020-01-28 13:53:03 +0200

    [UPDATE] implements TypeOrmCrudService
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
+import {Contacts} from "../entities/Contacts";
 
 @Injectable()
-export class ContactsService {}
+export class ContactsService extends TypeOrmCrudService<Contacts>{
+    constructor(@InjectRepository(Contacts) repo) {
+        super(repo);
+    }
+}
