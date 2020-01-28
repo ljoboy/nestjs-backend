57ce88be57fc23da39acff193cd825a7077aa3f2
Dark Angel <jonathanyombo@gmail.com>
2020-01-28 18:19:34 +0200

    [UPDATE] add typeorm feature
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/websites/websites.module.ts
+++ src/websites/websites.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WebsitesService } from './websites.service';
 import { WebsitesController } from './websites.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Websites} from "../entities/Websites";
 
 @Module({
   providers: [WebsitesService],
-  controllers: [WebsitesController]
+  controllers: [WebsitesController],
+  imports: [TypeOrmModule.forFeature([Websites])],
+  exports: [WebsitesService]
 })
 export class WebsitesModule {}
