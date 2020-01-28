e29728c5f5156d543dadad7830786b887f86b827
Dark Angel <jonathanyombo@gmail.com>
2020-01-28 13:40:03 +0200

    [UPDATE] implements crudController
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import {Crud, CrudController} from "@nestjsx/crud/";
+import {Contacts} from "../entities/Contacts";
+import {ContactsService} from "./contacts.service";
 
+@Crud({model: {
+        type: Contacts
+    }
+})
 @Controller('contacts')
-export class ContactsController {}
+export class ContactsController implements CrudController<Contacts>{
+    constructor(public service: ContactsService) {
+    }
+}
