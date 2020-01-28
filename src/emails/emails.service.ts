3f71bfa28e9ce23183e5ff08867d1220d263b012
Dark Angel <jonathanyombo@gmail.com>
2020-01-28 14:34:03 +0200

    [NEW] emails service
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

---
+++ src/emails/emails.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import {TypeOrmCrudService} from "@nestjsx/crud-typeorm";
+import {Emails} from "../entities/Emails";
+import {InjectRepository} from "@nestjs/typeorm";
+
+@Injectable()
+export class EmailsService extends TypeOrmCrudService<Emails>{
+    constructor(@InjectRepository(Emails) repo) {
+        super(repo);
+    }
+}
