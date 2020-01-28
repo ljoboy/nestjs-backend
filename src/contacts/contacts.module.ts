5210a455fc786a5d4e6128752b853ed61c5491fc
Dark Angel <jonathanyombo@gmail.com>
2020-01-28 13:47:22 +0200

    [UPDATE] add imports && exports
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { ContactsController } from './contacts.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Contacts} from "../entities/Contacts";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Contacts])],
   providers: [ContactsService],
+  exports: [ContactsService],
   controllers: [ContactsController]
 })
 export class ContactsModule {}
