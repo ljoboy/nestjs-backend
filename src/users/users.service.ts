0a3d377994220e46151d4605e1709970bd66933f
Dark Angel <jonathanyombo@gmail.com>
2020-01-28 22:35:44 +0200

    [NEW] users services
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import {TypeOrmCrudService} from "@nestjsx/crud-typeorm";
+import {Users} from "../entities/Users";
+import {InjectRepository} from "@nestjs/typeorm";
+
+@Injectable()
+export class UsersService extends TypeOrmCrudService<Users>{
+    constructor(@InjectRepository(Users) repo) {
+        super(repo);
+    }
+}
