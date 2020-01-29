9531d8053dc9c47ee00dc1186b535b72584664a9
Dark Angel <jonathanyombo@gmail.com>
2020-01-29 11:02:03 +0200

    [UPDATE] implements CrudController
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/websites/websites.controller.ts
+++ src/websites/websites.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import {Crud, CrudController} from "@nestjsx/crud";
+import {WebsitesService} from "./websites.service";
+import {Websites} from "../entities/Websites";
 
+@Crud({model: {
+        type: Websites
+    }
+})
 @Controller('websites')
-export class WebsitesController {}
+export class WebsitesController implements CrudController<Websites>{
+    constructor(public service: WebsitesService) {
+    }
+}
