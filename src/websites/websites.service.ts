36c4fe9430df626de12d1d0100adb492c277e83a
Dark Angel <jonathanyombo@gmail.com>
2020-01-29 11:01:08 +0200

    [UPDATE] extends TypeOrmCrudService
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/websites/websites.service.ts
+++ src/websites/websites.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import {TypeOrmCrudService} from "@nestjsx/crud-typeorm";
+import {Websites} from "../entities/Websites";
+import {InjectRepository} from "@nestjs/typeorm";
 
 @Injectable()
-export class WebsitesService {}
+export class WebsitesService extends TypeOrmCrudService<Websites>{
+    constructor(@InjectRepository(Websites) repo) {
+        super(repo);
+    }
+}
