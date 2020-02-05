8b25d0f199d090e842d1faea82834fc29c3c6a25
Dark Angel <jonathanyombo@gmail.com>
2020-02-05 17:49:09 +0200

    [UPDATE] change entity to repository
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { ContactsController } from './contacts.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {Contacts} from "./entities/Contacts";
 import {PassportModule} from "@nestjs/passport";
 import {JwtModule} from "@nestjs/jwt";
 import {jwtConstants} from "../auth/constants";
+import {ContactsRepository} from "./contacts.repository";
 
 @Module({
   imports: [
-  TypeOrmModule.forFeature([Contacts])],
+  TypeOrmModule.forFeature([ContactsRepository])],
   providers: [ContactsService],
   exports: [ContactsService],
   controllers: [ContactsController]
--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from "@nestjs/typeorm";
 import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
+import {ContactsRepository} from "./contacts.repository";
 import {Contacts} from "./entities/Contacts";
 
 @Injectable()
 export class ContactsService extends TypeOrmCrudService<Contacts>{
-    constructor(@InjectRepository(Contacts) repo) {
+    constructor(@InjectRepository(ContactsRepository) repo) {
         super(repo);
     }
 }
