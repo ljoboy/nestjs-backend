cb3a492ef067e23f4ec243cb66f5ca71c722801e
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 22:49:30 +0200

    [UPDATE] config jwt and passport
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UsersRepository} from "./users.repository";
+import {JwtModule} from "@nestjs/jwt";
+import {PassportModule} from "@nestjs/passport";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [
+      PassportModule.register({ defaultStrategy: 'jwt' }),
+      JwtModule.register({
+        secret: 'AllegrA1322',
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+      TypeOrmModule.forFeature([UsersRepository])
+  ],
   providers: [AuthService],
   exports: [AuthService],
   controllers: [AuthController],
