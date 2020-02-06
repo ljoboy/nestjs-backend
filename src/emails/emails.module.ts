90acefd79fb38f9f64ffdc2b69906c2af5b170b6
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 10:54:51 +0200

    [NEW] change emails entity to emails repo for feature
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/emails/emails.module.ts
+++ src/emails/emails.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EmailsController } from './emails.controller';
 import { EmailsService } from './emails.service';
-import {Emails} from "./entities/Emails";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {PassportModule} from "@nestjs/passport";
 import {JwtModule} from "@nestjs/jwt";
 import {jwtConstants} from "../auth/constants";
+import {EmailsRepository} from "./emails.repository";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Emails])],
+    TypeOrmModule.forFeature([EmailsRepository])],
   exports: [EmailsService],
   controllers: [EmailsController],
   providers: [EmailsService]
