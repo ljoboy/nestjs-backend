97dbe0af981ca8c94723f37f6f908fc9532368b9
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 17:37:40 +0200

    [UPDATE] delete jwt assets
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
-import { UsersModule } from '../users/users.module';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
-import { JwtStrategy } from './jwt.strategy';
+
+import { AuthController } from './auth.controller';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsersRepository} from "./users.repository";
 
 @Module({
-  imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '3600s' },
-    }),
-    UsersModule
-  ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  providers: [AuthService],
   exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
