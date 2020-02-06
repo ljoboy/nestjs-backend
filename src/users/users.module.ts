acc9cffec16751b111b299e87fd9dc1976144232
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 12:06:26 +0200

    [UPDATE] change feature
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {Users} from "./entities/Users";
+import {UsersRepository} from "./users.repository";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Users])],
+    TypeOrmModule.forFeature([UsersRepository])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
