588d4cdb62ff4b730f0029e80972c6ebec80fc2d
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 11:16:04 +0200

    [UPDATE] inject emails repo
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import {TypeOrmCrudService} from "@nestjsx/crud-typeorm";
 import {Emails} from "./entities/Emails";
 import {InjectRepository} from "@nestjs/typeorm";
+import {EmailsRepository} from "./emails.repository";
 
 @Injectable()
 export class EmailsService extends TypeOrmCrudService<Emails>{
-    constructor(@InjectRepository(Emails) repo) {
+    constructor(@InjectRepository(EmailsRepository) repo) {
         super(repo);
     }
 }
