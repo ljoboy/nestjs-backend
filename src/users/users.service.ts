29121ee3271356225ff5ef1a6876f36e5c313ae8
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 12:05:24 +0200

    [UPDATE] change injectrepo
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import {TypeOrmCrudService} from "@nestjsx/crud-typeorm";
 import {Users} from "./entities/Users";
 import {InjectRepository} from "@nestjs/typeorm";
+import {UsersRepository} from "./users.repository";
 
 @Injectable()
 export class UsersService extends TypeOrmCrudService<Users>{
-    constructor(@InjectRepository(Users) repo) {
+    constructor(@InjectRepository(UsersRepository) repo) {
         super(repo);
     }
 }
