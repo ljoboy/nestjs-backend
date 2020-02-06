702fcb0c62ce6fad4e24cb81204b6cc2b5b23875
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 17:38:44 +0200

    [UPDATE] add signup route
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthCredentialsDto} from "./dtos/auth-credentials.dto";
+import {AuthService} from "./auth.service";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {
+    }
+
+    @Post('/signup')
+    signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+        return this.authService.signUp(authCredentialsDto);
+    }
+}
