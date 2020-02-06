0b1455f074677b3ce1bd1753be1b3ef9a6974007
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 16:10:25 +0200

    [NEW] auth controller
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,7 +1,15 @@
 import {EntityRepository, Repository} from "typeorm";
 import {Users} from "./entities/Users";
+import {AuthCredentialsDto} from "../auth/dtos/auth-credentials.dto";
 
 @EntityRepository(Users)
 export class UsersRepository extends Repository<Users>{
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const {username, password} = authCredentialsDto;
+        const user = new Users();
+        user.username = username;
+        user.password = password;
 
+        await user.save();
+    }
 }
