432a0ed813b92b76f623619e47cd6ae20b12d125
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 17:09:16 +0200

    [UPDATE] remove users modules
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,9 @@ import { EmailsModule } from './emails/emails.module';
 import { PhonesModule } from './phones/phones.module';
 import { WebsitesModule } from './websites/websites.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import {PassportModule} from "@nestjs/passport";
-import {JwtModule} from "@nestjs/jwt";
-import {jwtConstants} from "./auth/constants";
 
 @Module({
-  imports: [ContactsModule, EmailsModule, PhonesModule, WebsitesModule, AuthModule, UsersModule,
+  imports: [ContactsModule, EmailsModule, PhonesModule, WebsitesModule, AuthModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db',
