643f0aae13cd6cfb6d5c35986553ab8fed2a6d75
Dark Angel <jonathanyombo@gmail.com>
2020-02-06 18:56:26 +0200

    [UPDATE] add class validator for auth-credentials dto
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/auth/dtos/auth-credentials.dto.ts
+++ src/auth/dtos/auth-credentials.dto.ts
@@ -1,4 +1,13 @@
+import {IsString, MaxLength, MinLength} from "class-validator";
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(3)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(40)
     password: string;
 }
