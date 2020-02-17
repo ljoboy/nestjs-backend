b8eb8be9059954cf74a8eb7840b7114e22078df9
Dark Angel <jonathanyombo@gmail.com>
2020-02-17 11:22:52 +0200

    [UPDATE] enable CORS in dev mode
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development')
+    app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
