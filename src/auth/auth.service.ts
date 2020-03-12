64a1d4bbd6e0fe2725ebca1c43c47b1036931422
Dark Angel <jonathanyombo@gmail.com>
2020-03-12 09:12:17 +0200

    [UPDATE] change auth-credentials to register-credentials
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import {Injectable, UnauthorizedException} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {UsersRepository} from "./users.repository";
-import {AuthCredentialsDto} from "./dtos/auth-credentials.dto";
+import {LoginCredentialsDto} from "./dtos/login-credentials.dto";
 import {JwtService} from "@nestjs/jwt";
 import {JwtPayload} from "./jwt-payload.interface";
+import {RegisterCredentialsDto} from "./dtos/register-credentials.dto";
 
 @Injectable()
 export class AuthService {
@@ -11,20 +12,21 @@ export class AuthService {
         @InjectRepository(UsersRepository)
         private usersRepository: UsersRepository,
         private jwtService: JwtService,
-    ) {}
-
-    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
-        return this.usersRepository.signUp(authCredentialsDto);
+    ) {
     }
 
-    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+    async signUp(registerCredentialsDto: RegisterCredentialsDto): Promise<void> {
+        return this.usersRepository.signUp(registerCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: LoginCredentialsDto): Promise<{ accessToken: string }> {
         const username = await this.usersRepository.validateUserPassword(authCredentialsDto);
 
         if (!username)
             throw new UnauthorizedException('Invalid credentials!');
 
-        const payload: JwtPayload = { username };
+        const payload: JwtPayload = {username};
         const accessToken = await this.jwtService.sign(payload);
-        return { accessToken };
+        return {accessToken};
     }
 }
