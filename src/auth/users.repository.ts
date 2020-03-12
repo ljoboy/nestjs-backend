d3f4d080e28c21b8dce4380ca2fc7a7d176073b3
Dark Angel <jonathanyombo@gmail.com>
2020-03-12 09:10:43 +0200

    [UPDATE] add missing attributes
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,22 +1,29 @@
 import {EntityRepository, Repository} from "typeorm";
 import {Users} from "./entities/Users";
-import {AuthCredentialsDto} from "./dtos/auth-credentials.dto";
+import {LoginCredentialsDto} from "./dtos/login-credentials.dto";
 import {ConflictException, InternalServerErrorException} from "@nestjs/common";
 import * as bcrypt from "bcrypt";
+import {RegisterCredentialsDto} from "./dtos/register-credentials.dto";
 
 @EntityRepository(Users)
-export class UsersRepository extends Repository<Users>{
-    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        const {username, password} = authCredentialsDto;
+export class UsersRepository extends Repository<Users> {
+    async signUp(registerCredentialsDto: RegisterCredentialsDto): Promise<void> {
+        const {username, password, address, country, email, full_name, genre, ville} = registerCredentialsDto;
 
         const user = new Users();
         user.username = username;
         user.salt = await bcrypt.genSalt();
         user.password = await this.hashPassword(password, user.salt);
+        user.address = address;
+        user.country = country;
+        user.email = email;
+        user.full_name = full_name;
+        user.genre = genre;
+        user.ville = ville;
 
         try {
             await user.save();
-        }catch (error) {
+        } catch (error) {
             if (error.code === 'SQLITE_CONSTRAINT')
                 throw new ConflictException('Username already taken');
             else
@@ -24,9 +31,9 @@ export class UsersRepository extends Repository<Users>{
         }
     }
 
-    async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    async validateUserPassword(authCredentialsDto: LoginCredentialsDto): Promise<string> {
         const {username, password} = authCredentialsDto;
-        const user = await this.findOne({ username });
+        const user = await this.findOne({username});
 
         if (user && await user.validatePassword(password))
             return user.username;
